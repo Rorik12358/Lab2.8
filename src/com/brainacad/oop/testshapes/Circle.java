e3a2cc202d7eda30828c6b53807e632d8c097c34
Roman Mazurov <mazurov.rn@gmail.com>
2016-03-16 00:11:34 +0200

    cleaned all not necessary this. and super.

--- src/com/brainacad/oop/testshapes/Circle.java
+++ src/com/brainacad/oop/testshapes/Circle.java
@@ -3,6 +3,7 @@ package com.brainacad.oop.testshapes;
 /**
  * Created by R2-D2 on 28.02.2016.
  */
+// cleaned all not necessary this. and super.
 public class Circle extends Shape implements Comparable{
     private double radius;
 
@@ -19,20 +20,20 @@ public class Circle extends Shape implements Comparable{
     @Override
     public String toString() {
         //“This is Circle, color: GREEN, radius=22”
-        return "This is Circle, color: "+super.getShapeColor()+", radius="+radius;  //you don't need super keyword
+        return "This is Circle, color: " + getShapeColor() + ", radius=" + radius;  //you don't need super keyword
     }
 
     @Override
     // This is Rectangle, color: RED, width=11, height=22,  area is: 242
     public void draw() {
-        System.out.println(this.toString()+" area is: "+calcArea());
+        System.out.println(toString() + " area is: " + calcArea());
     }
 
     @Override
     public int compareTo(Object o) {
         Circle circle = (Circle)o;
-        if(this.calcArea()>circle.calcArea())return 1;
-        if(this.calcArea()<circle.calcArea())return -1;
+        if (calcArea() > circle.calcArea()) return 1;
+        if (calcArea() < circle.calcArea()) return -1;
         return 0;
     }
 }
